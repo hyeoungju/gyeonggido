87bb798ad6e5ccae7282b5b3e52ebbc45fc04b5a
hyeoungju <103983311+hyeoungju@users.noreply.github.com>
2022-06-07 03:27:44 +0900

    ff
    ff

--- js/common.js
+++ js/common.js
@@ -1,4 +1,12 @@
 'use strict'
 $(function(){
+  $(".main_gnb").mouseover(function(){
+    $(".submenu").css("display","block").css("width","100%").css("background","#fff");
+    $(".gnbRight").css("display","block").css("width","100%").css("background","#f3f3f3");
+  });
+  $(".main_gnb").mouseout(function(){
+    $(".submenu").css("display","none");
+    $(".gnbRight").css("display","none");
+  });
 
 });
